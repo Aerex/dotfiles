0483100ee106dd63b7cfa1d07f41edc5e0c1525f
Aerex <nanaus13@yahoo.com>
2019-03-22 15:34:22 -0500

    feat(neomutt): add configs for neomutt
    '

---
+++ neomutt/.config/neomutt/view-attachment.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+TMP_MUTT_ATTACH_DIR=/tmp/mutt_attachment
+FILE_PATH=$1
+FILE_NAME=$(basename $FILE_PATH)
+OPEN_WITH_PROG=$2
+CONTENT_TYPE=$3
+
+# Create tmp attachment directory if it doesn't exist
+if [ ! -d /tmp/mutt_attachment ]; then
+  mkdir /tmp/mutt_attachment
+fi
+
+case $CONTENT_TYPE in
+  text/html )
+    FILE_TYPE='html'
+    ;;
+  *)
+    FILE_TYPE='txt'
+    ;;
+esac
+
+
+# Copy file so we don't lose it once neomutt deletes the original copy
+cp "$FILE_PATH" "$TMP_MUTT_ATTACH_DIR/$FILE_NAME.$FILE_TYPE"
+
+
+# Default to use xdg-open
+if [ -z $OPEN_WITH_PROG ]; then
+  xdg-open "$TMP_MUTT_ATTACH_DIR/$FILE_NAME.$FILE_TYPE"
+else
+  $OPEN_WITH_PROG "$TMP_MUTT_ATTACH_DIR/$FILE_NAME.$FILE_TYPE"
+  PROG=$(basename $OPEN_WITH_PROG)
+  echo "Opening with \"$PROG\""
+fi
+
+
+
