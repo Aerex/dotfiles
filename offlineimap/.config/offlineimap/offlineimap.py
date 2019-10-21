6f8b3f205eeddb32247a6400763dc74b34d79cd5
Aerex <nanaus13@yahoo.com>
2019-10-21 12:17:29 -0500

    feat: Added offlineimap conf

---
+++ offlineimap/.config/offlineimap/offlineimap.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python2
+from subprocess import check_output
+
+def get_pass(account):
+    return check_output('pass Email/' + account, shell=True).splitlines()[0]
+
+def get_user(account):
+    output = check_output('pass Email/' + account, shell=True).splitlines()
+    user = output[1].replace('login:', '').replace(' ', '')
+    return user
+
+def get_imap_host(account):
+    output = check_output('pass Email/' + account, shell=True).splitlines()
+    imap_host = output[3].replace('imap_domain:', '').replace(' ', '')
+    return imap_host
+
+def get_imap_port(account):
+    output = check_output('pass Email/' + account, shell=True).splitlines()
+    imap_port = output[5].replace('imap_port:', '').replace(' ', '')
+    return imap_port
+
