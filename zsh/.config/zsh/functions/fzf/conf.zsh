0903ee11f96834b8a991ddb7144fa72c3c3524d4
Aerex <nanaus13@yahoo.com>
2019-12-19 11:27:48 -0600

    fix: Use hardcoded conf dirs for conf binary

--- zsh/.config/zsh/functions/fzf/conf.zsh
+++ zsh/.config/zsh/functions/fzf/conf.zsh
@@ -1,11 +1,9 @@
 function conf(){
-  CONFIG_NAMES=(wuzz, tmux, nvim, jira, chunkwm, khd, kube, mycli, qutebrowser, taskwarrior, bugwarrior)
   CONFIG_DIRS=(~/.wuzz/config.toml, ~/.tmux.conf, ~/.config/nvim/init.vim, ~/.jira.d/config.yml, ~/.chunkwmrc, ~/.skhdrc, ~/.kube/config,
-  ~/.myclirc, ~/.qutebrowser/config.py, ~/.taskrc, ~/.config/bugwarrior/bugwarriorrc)
+  ~/.myclirc, ~/.qutebrowser/config.py, ~/.taskrc, ~/.config/bugwarrior/bugwarriorrc, ~/.config/vdirsyncer/config, ~/.config/tig/config, 
+  ~/tmux.powerline.conf, ~/.my.cnf, ~/.config/neomutt/muttrc, ~/.offlineimaprc)
+  VERT_CONFIG_DIRS=$(echo $CONFIG_DIRS | awk -v RS=, '{ sub(" ", ""); print }')
 
-  CONFIG_NAMES=$(jq -r 'keys[]' conf.json)
-  CONFIG_DIRS=$(jq -r  '.[]' conf.json)
-
-
-  fzf --preview="echo vimcat $(jq '.{}')"
+  echo $VERT_CONFIG_DIRS | fzf --preview="highlight -O ansi -l --force {}" \
+    --bind "enter:execute($EDITOR {})+abort"
 }
