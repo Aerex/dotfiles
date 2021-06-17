0c076af2739803c5c8f2fb330d5bf1f2f9d6c58c
Aerex <noamfo@aerex.me>
2021-06-16 22:27:52 -0500

    feat(nvim-0.5): Exported table set keymap for mappings

--- nvim-0.5/.config/nvim/lua/mappings.lua
+++ nvim-0.5/.config/nvim/lua/mappings.lua
@@ -1,13 +1,15 @@
+local M = {}
 local maps = {
     -- misc
     n = {
       -- misc
-      ['<leader>w'] = 'write',
-      ['<leader>fm'] = 'VsplitVifm',
+      ['<leader>w']  = 'write', ['<leader>fm'] = 'VsplitVifm',
       -- fzf
-      ['<leader>ff'] = 'lua require (\'nvim-fzf.files\')()',
-      ['<leader>fh'] = 'lua require (\'nvim-fzf.helptags\')()',
-      ['<leader>p'] = 'lua require (\'nvim-fzf.git\')()',
+      ['<leader>ff'] = 'lua require(\'nvim-fzf.files\')()',
+      ['<leader>fh'] = 'lua require(\'nvim-fzf.helptags\')()',
+      ['<leader>p']  = 'lua require(\'nvim-fzf.git\')()',
+      ['<leader>rg'] = 'lua require(\'nvim-fzf.rg\')()',
+      ['<leader>rG'] = 'lua require(\'nvim-fzf.rg\')(vim.fn.expand("<cword>"))',
       -- snippets
       ['<leader>ue'] = 'UltiSnipsEdit',
       -- git
@@ -15,16 +17,27 @@ local maps = {
       ['<leader>gd'] = 'Gdiff',
       ['<leader>gb'] = 'Git blame',
       ['<leader>gw'] = 'Gwrite',
+      ['<leader>gp'] = 'Git push'
+
       -- TODO: create map for git push --set-upstream current branch
     }
 }
 
--- Credits to https://github.com/bsuth/dots/blob/c6fc019b417d2a32e31f4099228ffa6d89944e81/nvim/lua/mappings.lua
-for mode, modebindings in pairs(maps) do
-  for lhs, rhs in pairs(modebindings) do
-    -- Append <cmd> and <cr> to better insert command
-    rhs = '<cmd>' .. rhs .. '<cr>'
-    vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+-- @param {table} m
+-- @param {table|nil} opts
+M.set_keymap = function(m, opts)
+  -- Credits to https://github.com/bsuth/dots/blob/c6fc019b417d2a32e31f4099228ffa6d89944e81/nvim/lua/mappings.lua
+  for mode, modebindings in pairs(m) do
+    for lhs, rhs in pairs(modebindings) do
+      -- Append <cmd> and <cr> to better for vim command
+      if not string.find(rhs, 'v:lua') then
+        rhs = '<cmd>' .. rhs .. '<cr>'
+      end
+      vim.api.nvim_set_keymap(mode, lhs, rhs, opts or { noremap = true, silent = true })
+    end
   end
 end
 
+M.set_keymap(maps)
+
+return M
