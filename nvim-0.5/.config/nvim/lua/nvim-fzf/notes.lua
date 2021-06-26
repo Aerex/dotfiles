8ecb37b12210102eefad17e82c0bf918449012bc
Aerex <noamfo@aerex.me>
2021-06-26 14:25:57 -0500

    feat(nvim-0.5): Added fzf-rg for getting notes and abstract some methods into a module

--- nvim-0.5/.config/nvim/lua/nvim-fzf/notes.lua
+++ nvim-0.5/.config/nvim/lua/nvim-fzf/notes.lua
@@ -1,26 +1,19 @@
--- Credits to https://github.com/vijaymarupudi/nvim-fzf-commands/blob/master/lua/fzf-commands/rg.lua
 require('fzf').default_window_options = {
   window_on_create = function()
     vim.cmd('set winhl=Normal:Normal')
   end
 }
-local action = require "fzf.actions".action
-local fzf = require('fzf').fzf
-local utils = require('nvim-fzf.utils')
 
-local function open_file(window_cmd, filename, row, col)
-  vim.cmd(window_cmd .. " ".. vim.fn.fnameescape(filename))
-  vim.api.nvim_win_set_cursor(0, {row, col - 1})
-  -- center the window
-  vim.cmd "normal! zz"
-end
+local fzf = require('fzf').fzf
+local action = require "fzf.actions".action
+local utils = require('nvim-fzf.utils')
 
 local has_bat = vim.fn.executable("bat")
 
 local preview_action = action(function (lines, fzf_lines)
   fzf_lines = tonumber(fzf_lines)
   local line = lines[1]
-  local parsed = utils.parse_vimgrep_line(line)
+  local parsed = utils.parse_line(line)
   if has_bat then
     return utils.bat_preview(parsed, fzf_lines)
   else
@@ -28,11 +21,16 @@ local preview_action = action(function (lines, fzf_lines)
   end
 end)
 
-return function(pattern)
+return function(pattern, opts)
   coroutine.wrap(function ()
-    local rgcmd = "rg --vimgrep --no-heading " ..
-      "--color ansi " .. vim.fn.shellescape(pattern or '')
-    local choices = fzf(rgcmd, "--multi --ansi --expect=ctrl-t,ctrl-s,ctrl-v " .. "--preview " .. preview_action)
+    opts = opts or {}
+    local notes_dir = opts.notes_dir or vim.fn.expand(vim.g.notes_dir)
+    local rgcmd = "rg --follow --ignore-files --smart-case --line-number --color never " ..
+    "--no-messages --no-heading --with-filename " .. vim.fn.shellescape(pattern or '') ..
+    " " .. vim.fn.shellescape(notes_dir)
+    local fzf_options = " --print-query --ansi --multi --info=inline --expect=ctrl-t,ctrl-s,ctrl-v " ..
+    "--preview " ..preview_action
+    local choices = fzf(rgcmd, fzf_options)
     if not choices then return end
 
     local window_cmd
@@ -50,7 +48,7 @@ return function(pattern)
     for i=2,#choices do
       local choice = choices[i]
       local parsed_content = utils.parse_vimgrep_line(choice)
-      open_file(window_cmd,
+      utils.open_file(window_cmd,
         parsed_content.filename,
         parsed_content.row,
         parsed_content.col)
---
+++ nvim-0.5/.config/nvim/lua/nvim-fzf/utils.lua
@@ -0,0 +1,63 @@
+local M = {}
+
+M.parse_vimgrep_line = function (line)
+  local parsed_content = {string.match(line, "(.-):(%d+):(%d+):.*")}
+  return M.get_line_parts(parsed_content)
+end
+
+M.parse_line = function(line)
+  local parsed_content = {string.match(line, "(.+):(%d+):(.*)")}
+  return M.get_line_parts(parsed_content)
+end
+
+M.get_line_parts = function(parsed_content)
+  local filename = parsed_content[1]
+  local row = tonumber(parsed_content[2])
+  local col = tonumber(parsed_content[3])
+  return {
+    filename = filename,
+    row = row,
+    col = col
+  }
+end
+
+M.get_preview_line_range = function(parsed, fzf_lines)
+  local line_start = parsed.row - (fzf_lines / 2)
+  if line_start < 1 then
+    line_start = 1
+  else
+    line_start = math.floor(line_start)
+  end
+
+  -- the minus one prevents an off by one error, because these are line INCLUSIVE
+  local line_end = math.floor(parsed.row + (fzf_lines / 2)) - 1
+
+  return line_start, line_end
+end
+
+M.bat_preview = function(parsed, fzf_lines)
+  local line_start, line_end = M.get_preview_line_range(parsed, fzf_lines)
+  local cmd = "bat --style=numbers --color always " .. vim.fn.shellescape(parsed.filename) ..
+    " --highlight-line " .. tostring(parsed.row) ..
+    " --line-range " .. tostring(line_start) .. ":" .. tostring(line_end)
+  return vim.fn.system(cmd)
+end
+
+M.open_file = function(window_cmd, filename, row, col)
+  vim.cmd(window_cmd .. " ".. vim.fn.fnameescape(filename))
+  vim.api.nvim_win_set_cursor(0, {row, col - 1})
+  -- center the window
+  vim.cmd "normal! zz"
+end
+
+M.head_tail_preview = function(parsed, fzf_lines)
+  local line_start, line_end = M.get_preview_line_range(parsed, fzf_lines)
+  local output =  vim.fn.systemlist("tail --lines=+" .. tostring(line_start) .. " " .. vim.fn.shellescape(parsed.filename) ..
+    "| head -n " .. tostring(line_end - line_start))
+
+  local row_index = parsed.row - (line_start - 1)
+  output[row_index] = "\x1B[1m\x1B[30m\x1B[47m" .. output[row_index] .. "\x1B[0m"
+  return output
+end
+
+return M
