8b61a421346dbef83d6a900e5b6085ed7e2fe827
Aerex <nanaus13@yahoo.com>
2021-07-05 15:16:03 -0500

    fix(nvim-0.5): Updated handled options for mapping function

--- nvim-0.5/.config/nvim/lua/mappings.lua
+++ nvim-0.5/.config/nvim/lua/mappings.lua
@@ -1,5 +1,5 @@
 local M = {}
-local maps = {
+local noremaps = {
     -- misc
     n = {
       -- misc
@@ -12,6 +12,7 @@ local maps = {
       ['<leader>rg'] = 'lua require(\'nvim-fzf.rg\')()',
       ['<leader>rG'] = 'lua require(\'nvim-fzf.rg\')(vim.fn.expand("<cword>"))',
       ['<leader>nv'] = 'lua require(\'nvim-fzf.notes\')()',
+      ['<leader>yp'] = 'lua require(\'nvim-fzf.yank-history\')()',
       -- snippets
       ['<leader>ue'] = 'UltiSnipsEdit',
       -- git
@@ -20,9 +21,15 @@ local maps = {
       ['<leader>gb'] = 'Git blame',
       ['<leader>gw'] = 'Gwrite',
       ['<leader>gp'] = 'Git push'
-
       -- TODO: create map for git push --set-upstream current branch
     }
+}
+
+local maps = {
+  n = {
+    p = '<Plug>(miniyank-autoput)',
+    P = '<Plug>(miniyank-autoPut)'
+  }
 }
 
 local file_type_keymaps = {
@@ -41,13 +48,14 @@ vim.api.nvim_set_keymap('n', '<leader><enter>', ':', { noremap = true, silent = 
 -- @param {table|nil} opts
 M.set_keymap = function(m, opts)
   -- Credits to https://github.com/bsuth/dots/blob/c6fc019b417d2a32e31f4099228ffa6d89944e81/nvim/lua/mappings.lua
+  opts = opts or { silent = true }
   for mode, modebindings in pairs(m) do
     for lhs, rhs in pairs(modebindings) do
-      -- Append <cmd> and <cr> to better for vim command
-      if not string.find(rhs, 'v:lua') then
+      -- Append <cmd> and <cr> to better for vim command (exclude <Plug> commands)
+      if not string.find(rhs, 'v:lua') and not string.find(rhs, '<Plug>') then
         rhs = '<cmd>' .. rhs .. '<cr>'
       end
-      vim.api.nvim_set_keymap(mode, lhs, rhs, opts or { noremap = true, silent = true })
+      vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
     end
   end
 end
@@ -63,6 +71,7 @@ M.set_filetype_keymap = function(m)
   end
 end
 
+M.set_keymap(noremaps, { silent = true, noremap = true })
 M.set_keymap(maps)
 M.set_filetype_keymap(file_type_keymaps)
 
