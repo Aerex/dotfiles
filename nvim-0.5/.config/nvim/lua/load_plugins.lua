beacd86aeb0b6ac76e866a1e8d2f0874441f5bee
Aerex <noamfo@aerex.me>
2021-07-07 11:38:50 -0500

    fix(nvim-0.5): Fixed plugin configs for packer

--- nvim-0.5/.config/nvim/lua/load_plugins.lua
+++ nvim-0.5/.config/nvim/lua/load_plugins.lua
@@ -8,7 +8,12 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Auto compile when there are changes in plugins.lua
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+vim.cmd 'autocmd BufWrite load_plugins.lua PackerCompile'
+
+-- Mappings for packer
+vim.cmd[[autocmd BufEnter load_plugins.lua noremap <leader>ps <cmd>PackerSync<cr>]]
+vim.cmd[[autocmd BufEnter load_plugins.lua noremap <leader>pc <cmd>PackerCompile<cr>]]
+vim.cmd[[autocmd BufEnter load_plugins.lua noremap <leader>pS <cmd>PackerStatus<cr>]]
 
 -- require('packer').init({display = {non_interactive = true}})
 require('packer').init({display = {auto_clean = false}})
@@ -17,47 +22,48 @@ local ok, _ = pcall(vim.cmd, [[packadd packer.nvim]])
 
 if ok then
   return require('packer').startup(
-    function(use)
-      -- Packer can manage itself as an optional plugin
-      use {'wbthomason/packer.nvim', opt = true}
+  function(use)
+    -- Packer can manage itself as an optional plugin
+    use {'wbthomason/packer.nvim'}
 
-      -- lsp
-      use {
-        'neovim/nvim-lspconfig',
-        'onsails/lspkind-nvim',
-       'nvim-lua/lsp-status.nvim',
-	      'ray-x/lsp_signature.nvim'
-      }
+    -- lsp
+    use {
+      'neovim/nvim-lspconfig',
+      'onsails/lspkind-nvim',
+      'nvim-lua/lsp-status.nvim',
+      'ray-x/lsp_signature.nvim'
+    }
 
-      -- treesitter
-      use {
-        { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', setup = [[require('treesitter')]] },
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        'nvim-treesitter/playground'
-      }
+     -- treesitter
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      requires = {
+        'nvim-treesitter/nvim-treesitter-refactor', 'nvim-treesitter/nvim-treesitter-textobjects', 'nvim-treesitter/playground'
+      },
+      run = ':TSUpdate'
+    }
+    -- textobject
+    use {
+      'blackCauldron7/surround.nvim', config = function() require('surround').setup{} end
+    }
 
-      -- textobject
-      use {
-        'blackCauldron7/surround.nvim'
-      }
+    -- fuzzy pickers / file finders
+    use {
+      'vijaymarupudi/nvim-fzf',
+      'vijaymarupudi/nvim-fzf-commands',
+      'vifm/vifm.vim'
+    }
 
-      -- fuzzy pickers / file finders
-      use {
-        'vijaymarupudi/nvim-fzf',
-        'vijaymarupudi/nvim-fzf-commands',
-        'vifm/vifm.vim'
-      }
-
-      -- git
-      use {
-        { 'tpope/vim-fugitive', cmd = {'Git', 'Gstatus', 'Gblame', 'Gpush', 'Gpull', 'Gwrite'} },
-        { 'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} },
-        -- still has some issues need to wait before using
-        { 'TimUntersberger/neogit', cmd = {'Neogit'}, requires = { 'nvim-lua/plenary.nvim','sindrets/diffview.nvim' }, opt = true },
-        { 'pwntester/octo.nvim',
-          cmd = { 'Octo' },
-          requires = { {'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}, {'nvim-telescope/telescope.nvim'} } }
-      }
+    -- git
+    use {
+      { 'tpope/vim-fugitive', cmd = {'Git', 'Gpush'} },
+      { 'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}, config = function() require('nvim-git').setup_signs() end },
+      { 'Aerex/neogit', branch = 'feat/config-split-direction', cmd = {'Neogit'}, config = function() require('nvim-git').setup_neogit() end,
+        requires = { 'nvim-lua/plenary.nvim','sindrets/diffview.nvim' }},
+      { 'pwntester/octo.nvim',
+      cmd = { 'Octo' },
+      requires = { {'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}, {'nvim-telescope/telescope.nvim'} } }
+    }
 
       -- yank/undo
       use {
@@ -67,45 +73,51 @@ if ok then
 
       -- diagnostics
       use { 'folke/trouble.nvim',
-        cmd = {'Trouble', 'TroubleToggle', 'TroubleClose', 'TroubleRefresh' }
-      }
+      cmd = {'Trouble', 'TroubleToggle', 'TroubleClose', 'TroubleRefresh' }
+    }
 
-      -- formatting / colors
-      use {
-        'norcalli/nvim-colorizer.lua',
-        'p00f/nvim-ts-rainbow',
-        'chriskempson/base16-vim',
-        'miyakogi/seiya.vim',  -- enable transparent background
-        'sbdchd/neoformat',
-        { 'godlygeek/tabular', cmd = { 'Tabularize' } },
-        { 'lukas-reineke/indent-blankline.nvim' }
-      }
+    -- formatting
+    use {
+      'sbdchd/neoformat',
+      { 'godlygeek/tabular', cmd = { 'Tabularize' } },
+      { 'lukas-reineke/indent-blankline.nvim' },
+    }
+    -- colors
+    use {
+      'norcalli/nvim-colorizer.lua',
+      'chriskempson/base16-vim',
+      'miyakogi/seiya.vim',  -- enable transparent background
+      { 'p00f/nvim-ts-rainbow', requires = { 'nvim-treesitter/nvim-treesitter' } }
+    }
 
-      -- debugger
-      --use {'mfussenegger/nvim-dap'}
+    -- debugger
+    --use {'mfussenegger/nvim-dap'}
 
-      -- statusline
-      use {'glepnir/galaxyline.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}, setup = [[require('statusline')]]}
+    -- statusline
+    use {'glepnir/galaxyline.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true},
+      config = function() require('statusline') end
+    }
 
-      -- markdown
-      use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+    -- markdown
+    use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install',
+      cmd = {'MarkdownPreview', 'MarkdownPreviewStop'} , ft = {'markdown'}
+    }
 
-      -- completions
-      use { 'hrsh7th/nvim-compe', setup = [[require('completion')]], event = 'InsertEnter' }
+   -- completions / snippets
+   use {
+     { 'hrsh7th/nvim-compe', event = 'InsertEnter *', config = function() require('completion') end },
+     {'SirVer/ultisnips', requires = {{'honza/vim-snippets' }}, setup = [[require('snippets')]] }
+   }
+    -- profiling
+    use { 'tweekmonster/startuptime.vim' }
 
-      -- snippets
-      use { 'SirVer/ultisnips', requires = {{'honza/vim-snippets' }}, setup = [[require('snippets')]] }
-
-      -- profiling
-      use { 'tweekmonster/startuptime.vim' }
-
-      -- marks
-      use { 'kshenoy/vim-signature' }
-      -- terminal
-      use {
-         'akinsho/nvim-toggleterm.lua',
-        config = function() require'toggleterm'.setup{ open_mapping = [[<C-t>]] } end
-       }
-    end
-    )
+    -- marks
+    use { 'kshenoy/vim-signature' }
+    -- terminal
+    use {
+      'akinsho/nvim-toggleterm.lua',
+     config = function() require'toggleterm'.setup{ open_mapping = [[<C-t>]], shading_factor = 1 } end
+    }
+  end
+  )
 end
