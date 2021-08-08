f4fa4a9789cda84240fc37178f2ca37e55adfba1
Aerex <noamfo@aerex.me>
2021-08-08 09:49:26 -0500

    feat(nvim-0.5): Added treesitter query linter

--- nvim-0.5/.config/nvim/lua/treesitter.lua
+++ nvim-0.5/.config/nvim/lua/treesitter.lua
@@ -15,6 +15,11 @@ require'nvim-treesitter.configs'.setup {
   playground = {
     enable = true
   },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = {"BufWrite", "CursorHold"},
+  },
   incremental_selection = {
     enable = true,
     disable = {},
