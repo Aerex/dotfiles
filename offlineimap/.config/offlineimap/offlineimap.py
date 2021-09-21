0628d5f4450ff7c96025112c6836d9fe821ce3d6
Aerex <nanaus13@yahoo.com>
2021-09-21 14:34:39 -0500

    fix(offlineimap): Loop through lines to get port from pass file

--- offlineimap/.config/offlineimap/offlineimap.py
+++ offlineimap/.config/offlineimap/offlineimap.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python2
 from subprocess import check_output
+import re
 
 def get_pass(account):
     output = check_output('pass Email/' + account, shell=True).splitlines()[0]
-    print('output {}'.format(output))
     return output
 
 def get_user(account):
@@ -12,12 +12,16 @@ def get_user(account):
     return user
 
 def get_imap_host(account):
-    output = check_output('pass Email/' + account, shell=True).splitlines()
-    imap_host = output[3].replace('imap_domain:', '').replace(' ', '')
-    return imap_host
+    lines = check_output('pass Email/' + account, shell=True).splitlines()
+    for line in lines:
+        if re.search("imap_domain", line):
+            imap_host = line.replace('imap_domain:', '').replace(' ', '')
+            return imap_host
 
 def get_imap_port(account):
-    output = check_output('pass Email/' + account, shell=True).splitlines()
-    imap_port = output[5].replace('imap_port:', '').replace(' ', '')
-    return imap_port
+    lines = check_output('pass Email/' + account, shell=True).splitlines()
+    for line in lines:
+        if re.search('imap_port', line):
+            imap_port = line.replace('imap_port:', '').replace(' ', '')
+            return imap_port
 
