d47f7c40114652518b34b15a22dd31ec29c4851c
Aerex <noamfo@aerex.me>
2021-11-12 09:37:55 -0600

    fix(zsh): Fixed httpj to not blow up if no json body was created in editor to POST

--- zsh/.config/zsh/functions/httpie/httpj.zsh
+++ zsh/.config/zsh/functions/httpie/httpj.zsh
@@ -3,6 +3,11 @@
 function httpj(){
   FILE_NAME=$(rand 40)
   TMPFILE=$(mktemp /tmp/$FILE_NAME.json)
+  NOT_EMPTY=$(cat TMPFILE | wc -c)
+  if [ $NOT_EMPTY = 0 ]; then
+    echo "content empty. exiting..." 
+    exit 0
+  fi
   nvim $TMPFILE
   http "$@" < $TMPFILE
   rm $TMPFILE
