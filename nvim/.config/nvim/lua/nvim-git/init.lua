f39bf14e8287df5fe3f55d9986884500a76b8899
Aerex <noamfo@aerex.me>
2022-07-30 17:37:48 -0500

    fix(nvim): Added check for local git cfgs

--- nvim/.config/nvim/lua/nvim-git/init.lua
+++ nvim/.config/nvim/lua/nvim-git/init.lua
@@ -74,43 +74,9 @@ M.setup_signs = function()
   --vim.cmd[[hi GitSignsAdd guifg=#afd700 guibg=None]]
   --vim.cmd[[hi GitSignsChange guifg=#EBCB8B guibg=None]]
 
-local ok_d, diffview = pcall(require, 'diffview')
-if ok_d then
-  local cb = require('diffview.config').diffview_callback
-  require('diffview').setup({
-      diff_binaries = false,
-      use_icons = true,
-      file_panel = {
-          width = 35,
-      },
-      key_bindings = {
-          view = {
-              ['<tab>'] = cb('select_next_entry'),
-              ['<s-tab>'] = cb('select_prev_entry'),
-              ['<leader>f'] = cb('focus_files'),
-              ['<esc>'] = cb('focus_files'),
-          },
-          file_panel = {
-              ['j'] = cb('next_entry'),
-              ['<down>'] = cb('next_entry'),
-              ['k'] = cb('prev_entry'),
-              ['<up>'] = cb('prev_entry'),
-              ['<cr>'] = cb('select_entry'),
-              ['o'] = cb('select_entry') .. '<cmd>sleep 100m<cr><cmd>DiffviewToggleFiles<cr>',
-              ['p'] = cb('select_entry') .. '<cmd>DiffviewFocusFiles<cr>',
-              ['<2-LeftMouse>'] = cb('select_entry'),
-              ['-'] = cb('toggle_stage_entry'),
-              ['S'] = cb('stage_all'),
-              ['U'] = cb('unstage_all'),
-              ['XX'] = cb('restore_entry'),
-              ['R'] = cb('refresh_files'),
-              ['<s-tab>'] = cb('select_prev_entry'),
-              ['<leader>F'] = cb('focus_files'),
-              ['<leader>f'] = cb('toggle_files'),
-              ['<esc>'] = '<cmd>DiffviewClose<cr>',
-          },
-      },
-  })
+  local ok_d, diffview = pcall(require, 'diffview')
+  if ok_d then
+    diffview.setup{}
   end
 end
 
@@ -168,8 +134,7 @@ end
 M.setup_octo = function()
   local octo_cfg = {}
   local cfg
-  local ok, lcfg = pcall(require, 'nvim-local')
-  if ok and lcfg['get_octo_config'] then
+  if okl and lcfg['get_octo_config'] then
     cfg = vim.tbl_deep_extend("force", octo_cfg, lcfg.get_octo_config())
   else
     cfg = octo_cfg
