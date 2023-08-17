4d650745ba61a853c52412ca20a040bb5e813440
Aerex <noamfo@aerex.me>
2023-08-16 21:41:17 -0400

    feat(nvim): Updated gitsigns keymap

--- nvim/.config/nvim/lua/nvim-git/init.lua
+++ nvim/.config/nvim/lua/nvim-git/init.lua
@@ -4,71 +4,86 @@ M.setup_signs = function()
   local ok, gitsigns = pcall(require, 'gitsigns')
   if ok then
     gitsigns.setup {
-      signs = {
-        add = {hl = 'GitSignsAdd', text = '+'},
-        change = {hl = 'GitSignsChange', text = '~'},
-        delete = {hl = 'GitSignsDelete',  text = "▂" },
-        topdelete = {hl = 'GitSignsDelete',  text = "▔" },
-        changedelete = {hl = 'GitSignsChange', text = '▎'},
+      on_attach                    = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        -- Actions
+        map('n', '<leader>hs', gs.stage_hunk)
+        map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        map('n', '<leader>hr', gs.reset_hunk)
+        map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        map('n', '<leader>hS', gs.stage_buffer)
+        map('n', '<leader>hu', gs.undo_stage_hunk)
+        map('n', '<leader>hR', gs.reset_buffer)
+        map('n', '<leader>hp', gs.preview_hunk)
+        map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>hd', gs.diffthis)
+        map('n', '<leader>hD', function() gs.diffthis('~') end)
+        map('n', '<leader>td', gs.toggle_deleted)
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      end,
+      signs                        = {
+        add = { hl = 'GitSignsAdd', text = '+' },
+        change = { hl = 'GitSignsChange', text = '~' },
+        delete = { hl = 'GitSignsDelete', text = "▂" },
+        topdelete = { hl = 'GitSignsDelete', text = "▔" },
+        changedelete = { hl = 'GitSignsChange', text = '▎' },
       },
-      keymaps = {
-        -- Default keymap options
-        noremap = true,
-        buffer = true,
-
-        ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-        ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
-
-        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-        ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-        ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line({full = true })<CR>',
-        ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-        ['n <leader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
-        ['n <leader>ht'] = '<cmd>lua require"gitsigns".toggle_signs()<CR>',
-
-        -- Text objects
-        ['o ih'] = '<cmd>lua require"gitsigns.actions".select_hunk()<CR>',
-        ['x ih'] = '<cmd>lua require"gitsigns.actions".select_hunk()<CR>'
-      },
-
-      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-      numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-      word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-      watch_gitdir = {
+      signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+      numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir                 = {
         interval = 1000,
         follow_files = true
       },
-      attach_to_untracked = true,
-      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
+      attach_to_untracked          = true,
+      current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts      = {
         virt_text = true,
         virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
         delay = 250,
         ignore_whitespace = false,
       },
       current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-      sign_priority = 1,
-      update_debounce = 100,
-      status_formatter = nil, -- Use default
-      max_file_length = 40000,
-    preview_config = {
-      -- Options passed to nvim_open_win
-      border = 'single',
-      style = 'minimal',
-      relative = 'cursor',
-      row = 0,
-      col = 1
-    },
-    yadm = {
-      enable = false
-    },
-  }
+      sign_priority                = 1,
+      update_debounce              = 100,
+      status_formatter             = nil, -- Use default
+      max_file_length              = 40000,
+      preview_config               = {
+        -- Options passed to nvim_open_win
+        border = 'single',
+        style = 'minimal',
+        relative = 'cursor',
+        row = 0,
+        col = 1
+      },
+      yadm                         = {
+        enable = false
+      },
+    }
   end
   -- override color highlights
   --vim.cmd[[hi GitSignsAdd guifg=#afd700 guibg=None]]
