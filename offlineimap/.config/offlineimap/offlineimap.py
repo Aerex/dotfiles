31e5d8d19e2506c407944ee3f83716bd05f8faca
Aerex <noamfo@aerex.me>
2024-07-22 10:06:34 -0400

    fix(offlineimap): fixed encoding when reading cred lines

--- offlineimap/.config/offlineimap/offlineimap.py
+++ offlineimap/.config/offlineimap/offlineimap.py
@@ -1,25 +1,33 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python3
 from subprocess import check_output
 import re
 
+
 def get_pass(account):
-    output = check_output('pass Email/' + account, shell=True).splitlines()[0]
+    output = check_output('pass Email/' + account,
+                          shell=True, encoding='utf-8').splitlines()[0]
     return output
 
+
 def get_user(account):
-    lines = check_output('pass Email/' + account, shell=True).splitlines()
+    lines = check_output('pass Email/' + account,
+                         shell=True, encoding='utf-8').splitlines()
     for line in lines:
         if re.search('login:', line):
             return line.replace('login:', '').replace(' ', '')
 
+
 def get_imap_host(account):
-    lines = check_output('pass Email/' + account, shell=True).splitlines()
+    lines = check_output('pass Email/' + account,
+                         shell=True, encoding='utf-8').splitlines()
     for line in lines:
         if re.search('imap_domain:', line):
             return line.replace('imap_domain:', '').replace(' ', '')
 
+
 def get_imap_port(account):
-    lines = check_output('pass Email/' + account, shell=True).splitlines()
+    lines = check_output('pass Email/' + account,
+                         shell=True, encoding='utf-8').splitlines()
     for line in lines:
         if re.search('imap_port:', line):
-            return line.replace('imap_port:', '').replace(' ', '')
+            return int(line.replace('imap_port:', '').replace(' ', ''))
