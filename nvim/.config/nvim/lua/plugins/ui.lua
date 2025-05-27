502490e140dd59c9b162c36ddfe95d364e9f45ed
Aerex <noamfo@aerex.me>
2025-05-27 13:12:31 -0400

    chore(nvim): added missing ui config

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -8,49 +8,9 @@ return
     end
   },
   {
-    'glepnir/galaxyline.nvim',
-    dependencies = { 'kyazdani42/nvim-web-devicons' },
-    config = function() require('plugins.configs.ui').galaxyline() end
-  },
-  {
     'AndrewRadev/bufferize.vim',
     cmd = { 'Bufferize' }
   },
-  {
-    'kevinhwang91/nvim-bqf',
-    dependencies = {
-      'junegunn/fzf',
-      build = ':call fzf#install()'
-    },
-    ft = 'qf',
-    cmd = 'QuickFixCmdPost',
-    opts = {
-      func_map = {
-        tab = '<C-t>',
-        split = '<C-s>',
-        vsplit = '<C-v>',
-        stoggleup = 'K',
-        stoggledown = 'J',
-
-        ptoggleitem = 'p',
-        ptoggleauto = 'P',
-        ptogglemode = 'zp',
-
-        pscrollup = '<C-b>',
-        pscrolldown = '<C-f>',
-
-        prevfile = '[f',
-        nextfile = ']f',
-
-        prevhist = '<C-p>',
-        nexthist = '<C-n>',
-      }
-    },
-    config = function()
-      require 'qf'
-    end
-  },
-
   {
     'rcarriga/nvim-notify',
     config = function()
