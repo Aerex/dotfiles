f3601e577a9cbef2c2fea04da76bb93fbf64360c
Aerex <noamfo@ibm.com>
2025-09-15 10:39:09 -0400

    refactor(zsh): consolidate taskwarrior file methods into one file

--- zsh/.config/zsh/functions/parent-ranger.zsh
+++
@@ -1,9 +0,0 @@
-RANGER_PROMPT="%{$fg[green]%}[R]%{$reset_color%}"
-function parent-ranger(){
- if [ ! -z $RANGER_LEVEL ]; then
-    echo $RANGER_PROMPT 
-  else
-    echo ""
-  fi
-}
-
---
+++ zsh/.config/zsh/functions/task.zsh
@@ -0,0 +1,93 @@
+# task context define <NAME> <FILTER>
+function tctx(){
+  NAME=$1 
+  FILTER=$2
+
+  if [ -z $NAME ]; then
+    echo "Missing a name for context"
+    return 1;
+  elif [ -z $FILTER ]; then
+    echo "Missing filter"
+    return 1;
+  fi
+  task context define $NAME $FILTER 
+}
+
+function tdn(){
+  TASK_NUMBER=$1
+
+  if [[  -z TASK_NUMBER ]]; then
+    echo "Missing task number"
+    return 1
+  fi
+
+  task $TASK_NUMBER denote
+
+}
+
+# task entry.after:now-<TIME-FILTER> <LIST-TYPE>
+function _teb_help(){
+  echo "usage: teb <time-fitler> [<list-type>]\n"
+  echo "<time-filter> The amount of time to go back for tasks\n"
+  echo "<list-type>  How the list should be returned. Default: \"ls\\n"
+}
+
+function teb(){
+  TIME_FILTER=$1 
+  LIST_TYPE=$2
+
+  if [ -z $TIME_FILTER ]; then
+    printf "Error: Missing time\n"
+    _teb_help
+    return 1;
+  elif [ -z $LIST_TYPE ]; then
+    LIST_TYPE=ls
+  fi
+  task entry.after:now-"${TIME_FILTER}" "${LIST_TYPE}" 
+}
+
+# task entry.after:now-<TIME-FILTER> <LIST-TYPE>
+function _tmon_help(){
+  echo "usage: tmon <TASK-ID>"
+  echo "<TASK-ID> The task id\n"
+}
+function tmon(){
+  TASK_ID=$1 
+
+  if [ -z $TASK_ID ]; then
+    printf "Error: Missing TASK_ID\n"
+    _tmon_help
+    return 1;
+  fi
+  task modify $TASK_ID -in +next 
+}
+
+function ttk(){
+  TASK_NUMBER=$1
+
+  if [[ -z $TASK_NUMBER ]]; then
+      'Missing task number'
+      return 1
+  fi
+
+  task modify $TASK_NUMBER -in +next
+
+}
+
+function tw(){
+  TASK=$1
+  TIME=$2
+
+  if [ -z $TASK ]; then
+    echo 'Need a task'
+    exit 1
+  fi
+
+  if [ -z $TIME ]; then
+    echo 'Need a time'
+    exit 1
+  fi
+
+  task modify $TASK wait:$TIME "$@"
+
+}
